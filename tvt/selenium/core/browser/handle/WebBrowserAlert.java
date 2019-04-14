7d0aa065de31e48c187c2618ca0f62de2453c0b2
Taylor <cytaylorw@gmail.com>
2019-04-14 18:53:22 +0800

    Remove org.openqa.selenium.security
    removed in Selenium 3

--- tvt/selenium/core/browser/handle/WebBrowserAlert.java
+++ tvt/selenium/core/browser/handle/WebBrowserAlert.java
@@ -1,15 +1,12 @@
 package tvt.selenium.core.browser.handle;
 
 import org.openqa.selenium.Alert;
-import org.openqa.selenium.security.Credentials;
-import org.openqa.selenium.security.UserAndPassword;
 
 import tvt.selenium.core.browser.WebBrowser;
 
 public class WebBrowserAlert {
 
 	private Alert alert;
-	private Credentials credential;
 	
 	/**
 	 * The constructor will switch to the alert.
@@ -32,34 +29,7 @@ public class WebBrowserAlert {
 	public void dismiss() {
 		alert.dismiss();
 	}
-	
-	/**
-	 * Set the credential to be used in the browser alert.
-	 * @param username User name for the credential.
-	 * @param password Password for the credential.
-	 * @return {@link WebBrowserAlert}
-	 */
-	public WebBrowserAlert setCredential(String username, String password){
-		credential = new UserAndPassword(username,password);
-		return this;
-	}
-	
-	/**
-	 * Authenticate using the provided credential.
-	 * @param username User name for the credential.
-	 * @param password Password for the credential.
-	 */
-	public void authenticate(String username,String password){
-		alert.authenticateUsing(new UserAndPassword(username,password));
-	}
-	
-	/**
-	 * Authenticate using the credential configured with {@link #setCredential(String, String)}.
-	 */
-	public void authenticate(){
-		alert.authenticateUsing(credential);
-	}
-	
+
 	/**
 	 * Send keys to the alert.
 	 * @param keys Keys to be sent to the alert.
